94a976b1e6c2457df8edc5946b25212032cd9d10
Muhammad Abdiel Firjatullah <abdielfirdie@gmail.com>
2022-04-08 21:40:29 +0700

    update file register.dart

--- lib/register_page.dart
+++ lib/register_page.dart
@@ -19,8 +19,120 @@ class RegisterPage extends StatelessWidget {
                     borderRadius: BorderRadius.circular(10),
                     color: const Color(0xffEEEEEE),
                   ),
-                  child: Image.asset('assets/icons/png/taskly_logo.png'),
-                )
+                  child: Image.asset(
+                    'assets/icons/png/taskly_logo.png',
+                  ),
+                ),
+                const SizedBox(height: 64),
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 32,
+                    vertical: 8,
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: const Color(0xffEEEEEE),
+                  ),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'First Name',
+                      hintStyle: TextStyle(
+                        fontFamily: 'Raleway',
+                        color: Color.fromARGB(255, 118, 118, 118),
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 18),
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 32,
+                    vertical: 8,
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: const Color(0xffEEEEEE),
+                  ),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Last Name (Optional)',
+                      hintStyle: TextStyle(
+                        fontFamily: 'Raleway',
+                        color: Color.fromARGB(255, 118, 118, 118),
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 18),
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 32,
+                    vertical: 8,
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: const Color(0xffEEEEEE),
+                  ),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Email',
+                      hintStyle: TextStyle(
+                        fontFamily: 'Raleway',
+                        color: Color.fromARGB(255, 118, 118, 118),
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 18),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/images/male-user-1.png',
+                      height: 70,
+                    ),
+                    Image.asset(
+                      'assets/images/female-user-1.png',
+                      height: 70,
+                    ),
+                  ],
+                ),
+                const Spacer(),
+                Center(
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: 60,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: const Color(0xffFFC045),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                      ),
+                      onPressed: () {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const RegisterPage(),
+                          ),
+                        );
+                      },
+                      child: const Text(
+                        'Start',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontFamily: 'Raleway',
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
